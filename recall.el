b8f8565df8bd62a057294b27352ec5eafdf9f8f4
Daniel Pettersson <daniel@dpettersson.net>
2024-10-29 21:58:20 +0100

    Update description

--- recall.el
+++ recall.el
@@ -27,24 +27,31 @@
 
 ;;; Commentary:
 
-;; This package `advice-add's `make-process' and friends to store
-;; metadata like; working directory, stdout, start time, end time,
-;; exit code and version control revision for Emacs subprocesses.
+;; Recall and rerun processes created by the likes of `eshell',
+;; `async-shell-command', `compile' and
+;; `dired-do-async-shell-command'.
 
-;; It defines commands to view and manage both currently running
-;; processes and historical processes, accessible via the minibuffer
-;; and a specialized `tabulated-list' buffer.
+;; Use `recall-list' to see working directory, stdout, start time, end
+;; time, exit code and vc revision for live and exited processes.
 
-;; Joining the functionality of bash reverse-i-search with `proced'.
+;; `recall-rerun' to rerun any process.
+;; `recall-rerun-edit' to rerun process after editing shell command.
+;; `recall-find-log' to open process output log file.
+;; `recall-process-kill' to kill running process.
+;; ...
 
-;; Enable process surveillance with `recall-mode'.
+;; Joining the functionality of bash reverse-i-search with `proced'
+;; (for subprocesses of Emacs).
 
 ;; Includes integration with `embark' and `consult'.
 
+;; Enable global mode `recall-mode' to start processes monitoring.
+
 ;; Note:
-;; As this package advices core functionality, usage might have
-;; unintended consequences.  Disable `recall-mode' at the
-;; first signs of process spawning troubles.
+;; This package uses `add-advice' on `make-process' and friends to
+;; store metadata.  This is core Emacs functionality, usage might
+;; have unintended consequences.  Disable `recall-mode' at the first
+;; signs of process spawning troubles.
 
 ;; Package is inspired by detached.el
 
